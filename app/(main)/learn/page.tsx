bd7abe7eb8f71fea00cb5a0a4079d7e6b687004c
Ayush Saini <ayush.saini.21cse@bmu.edu.in>
2024-09-11 01:58:03 +0530

    wrappers

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,7 +1,17 @@
+import { FeedWrapper } from "@/components/feed-wrapper";
+import { StickyWrapper } from "@/components/sticky-wrapper";
+import { Header } from "./header";
+import { UserProgess } from "@/components/user-progess";
+
 const LearnPage = ()=>{
     return(
-        <div>
-            <h1>Learn Page</h1>
+        <div className="flex flex-row-reverse gap-[48px] px-6">
+            <StickyWrapper>
+                <UserProgess activeCourse={{title: "Spanish", imageSrc : "/es.svg"}} hearts={5} points={100} hasActiveSubscription = {false}/>
+            </StickyWrapper>
+            <FeedWrapper>
+                <Header title="Spanish"/>
+            </FeedWrapper>
         </div>
     )
 }
